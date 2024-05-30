24abd7bc93ddce186a613a14940190be3af4081d
Guillermo Galindo <guillegalindo45@gmail.com>
2024-05-30 13:07:26 -0600

    Add new components to app module and routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactameComponent } from './contactame/contactame.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'sobre-mi', component: SobreMiComponent },
+  { path: 'habilidades', component: HabilidadesComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'contactame', component: ContactameComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/inicio' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactameComponent } from './contactame/contactame.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SobreMiComponent,
+    HabilidadesComponent,
+    ContactameComponent,
+    ProyectosComponent,
+
   ],
   imports: [
     BrowserModule,
