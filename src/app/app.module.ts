66e9f2d2598858ada9c96a7bd47c4d8f17a99b2a
Guillermo <guillegaindo45@gmail.com>
2024-10-24 19:28:06 -0600

    Add pwa package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
 import { HabilidadesComponent } from './habilidades/habilidades.component';
 import { ContactameComponent } from './contactame/contactame.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -22,7 +23,13 @@ import { ProyectosComponent } from './proyectos/proyectos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
